18094fdd9f482ffc5014f2f3139f0f9627bbd360
SJ <vince15dk@gmail.com>
2019-01-30 19:57:24 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Bye there!
+            SJ You can do it !!
           </p>
           <a
             className="App-link"
